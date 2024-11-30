62236f84ef17bdf77c44df80a1fc00295a42326d
DELL <DELL@DESKTOP-MKJPT94>
2024-11-30 21:19:52 +0530

    Add project files.

---
+++ SrinuMarket/Data/ApplicationDbContext.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace SrinuMarket.Data
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options)
+        {
+
+        }
+    }
+}
---
+++ SrinuMarket/Models/Market1.cs
@@ -0,0 +1,15 @@
+﻿namespace SrinuMarket.Models
+{
+    public class Market1
+    {
+        public Guid Id { get; set; }
+        public string Name { get; set; }
+        public string Description { get; set; }
+        public string Address { get; set; }
+        public string City { get; set; }
+
+        public string Region { get; set; }
+        public string PostalCode { get; set; }
+
+    }
+}
